2e35c3ed69ddbbc650c23d680157e97f81d8339a
JosueEmanuelDionicioOrellana <Jdionicioo1@miumg.edu.gt>
2024-06-01 10:36:47 -0600

    se hizo

--- Frontend/inventory_management_system/src/components/About.js
+++ Frontend/inventory_management_system/src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function About() {
   return (
     <div className='container-fluid p-5'>
-        <h1>Administrador de Productos - MERN CRUD by Yefrin Pérez</h1>
+        <h1>Administrador de Productos - MERN CRUD</h1>
     </div>
   )
 }
